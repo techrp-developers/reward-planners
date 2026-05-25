8c3b808967488b46a9c3d45c8d0af395845866e4
policyplanner1 <it@policyplanner.com>
2026-05-25 12:47:35 +0530

    app settings updated

--- server/app/common/routes/settingRoute.js
+++ server/app/common/routes/settingRoute.js
@@ -6,4 +6,7 @@ const SettingController = require("../controller/settingController");
 // All settings
 router.get("/app-settings", SettingController.getAppSettings);
 
+// Company Details
+router.get("/company-details", SettingController.getCompanyDetails);
+
 module.exports = router;
