34797d14d9b80edc10a5371ef41d04af32ff5303
policyplanner1 <it@policyplanner.com>
2026-05-26 15:29:42 +0530

    service phase-23

--- server/app/service/v1/routes/serviceCheckoutRoute.js
+++ server/app/service/v1/routes/serviceCheckoutRoute.js
@@ -3,30 +3,37 @@ const router = express.Router();
 const ServiceCheckoutController = require("../controllers/serviceCheckoutController");
 const auth = require("../../../common/middlewares/auth");
 
+// get cart checkout Details
+router.get(
+  "/checkout-preview",
+  auth,
+  ServiceCheckoutController.getCheckoutPreview,
+);
+
 // add to checkout
 router.post("/cart", auth, ServiceCheckoutController.addToCheckout);
 
 // bundle checkout
 // router.post("/bundle", auth, ServiceCheckoutController.bundleCheckout);
 
+// Get buy now checkout Details
+router.get(
+  "/buy-now-preview",
+  auth,
+  ServiceCheckoutController.getBuyNowPreview,
+);
+
 // buy now
 router.post("/buy-now", auth, ServiceCheckoutController.buyNow);
-
-// buy now bundle
-router.post("/buy-now-bundle", auth, ServiceCheckoutController.buyNowBundle);
-
-// get cart checkout Details
-router.get("/checkout-preview", auth, ServiceCheckoutController.getCheckoutPreview);
-
-// Get buy now checkout Details
-router.get("/buy-now-preview", auth, ServiceCheckoutController.getBuyNowPreview);
 
 // Get buy now bundle preview
 router.post(
   "/buy-now-bundle-preview",
   auth,
-  ServiceCheckoutController.getBuyNowBundlePreview
+  ServiceCheckoutController.getBuyNowBundlePreview,
 );
 
+// buy now bundle
+router.post("/buy-now-bundle", auth, ServiceCheckoutController.buyNowBundle);
 
 module.exports = router;
