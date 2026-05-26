09c2e42aefe5f0deed1133196435a0d5cc35a692
policyplanner1 <it@policyplanner.com>
2026-05-26 12:36:07 +0530

    ticket Generation

--- server/mps-connect/common/controller/authController.js
+++ server/mps-connect/common/controller/authController.js
@@ -50,6 +50,77 @@ class AuthController {
       });
     }
   }
+
+  // create support ticket
+  async createTicket(req, res) {
+    try {
+      const apiClientId = req.client.api_client_id;
+      const userId = req.body?.user_id;
+
+      if (!userId) {
+        return res.status(403).json({
+          success: false,
+          message: "Unauthorized user",
+        });
+      }
+
+      const {
+        subject,
+        description,
+        category_id,
+        // attachment_url,
+      } = req.body;
+
+      // validation
+      if (!subject || !description || !category_id) {
+        return res.status(400).json({
+          success: false,
+          message: "subject, description and category_id are required",
+        });
+      }
+
+      // check for existing open ticket in the same category
+      const [existing] = await db.execute(
+        `SELECT ticket_id 
+         FROM external_support_tickets 
+         WHERE user_id = ?
+         AND client_id = ?
+           AND category_id = ?
+           AND status IN ('open', 'in_progress')
+         LIMIT 1`,
+        [userId, apiClientId, category_id],
+      );
+
+      if (existing.length > 0) {
+        return res.status(200).json({
+          success: false,
+          message: "You already have an active request for this issue",
+          existing_ticket_id: existing[0].ticket_id,
+        });
+      }
+
+      const [result] = await db.execute(
+        `INSERT INTO external_support_tickets 
+         (client_id, user_id, subject, description, category_id, attachment_url)
+         VALUES (?, ?, ?, ?, ?, ?)`,
+        [apiClientId, userId, subject, description, category_id, null],
+      );
+
+      const ticketId = result.insertId;
+
+      return res.status(201).json({
+        success: true,
+        message: "Ticket created successfully",
+        ticket_id: result.insertId,
+      });
+    } catch (error) {
+      console.error("Create ticket error:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  }
 }
 
 module.exports = new AuthController();
