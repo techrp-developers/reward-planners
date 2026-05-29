f273581c4bb086f660ffedcb719188e97fd1b157
policyplanner1 <it@policyplanner.com>
2026-05-29 12:19:23 +0530

    mps phase-15

--- server/mps-connect/common/controller/serviceController.js
+++ server/mps-connect/common/controller/serviceController.js
@@ -1723,8 +1723,8 @@ class ServiceController {
   async cancellationDetails(req, res) {
     try {
       const apiClientId = req.client.api_client_id;
-      // const userId = req.query?.user_id;
-      const userId = 1;
+      const userId = req.query?.user_id;
+      // const userId = 1;
 
       if (!userId) {
         return res.status(403).json({
