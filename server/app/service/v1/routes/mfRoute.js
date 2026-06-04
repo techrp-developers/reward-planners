47b736e0e54e304df0cbda9ef21d9e32614dc517
policyplanner1 <it@policyplanner.com>
2026-06-04 17:56:36 +0530

    mf phase-3

--- server/app/service/v1/routes/mfRoute.js
+++ server/app/service/v1/routes/mfRoute.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const mfController = require("../controllers/mfController");
 const auth = require("../../../common/middlewares/auth");
 
+// Admin create section
+router.post("/create-section", mfController.createSection);
+
 // Get section
 router.get("/by-category/:categoryId", mfController.getSectionsByCategory);
-
-// Admin create section
-router.post("/create", mfController.createSection);
 
 module.exports = router;
