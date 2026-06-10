84b49e90a18b08c9089f19aae954b5e1cc4a2884
policyplanner1 <it@policyplanner.com>
2026-06-10 12:31:02 +0530

    maintenance phase-6

--- server/app/ecommerce/v1/routes/paymentRoute.js
+++ server/app/ecommerce/v1/routes/paymentRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const PaymentController = require("../controllers/paymentController");
+const drainMode = require("../../../../middleware/drainMode");
 
 // create payment
-router.post('/create-order', PaymentController.createOrder);
+router.post("/create-order", drainMode, PaymentController.createOrder);
 
 // verify payment
-router.post('/verify-payment', PaymentController.verifyPayment);
+router.post("/verify-payment", PaymentController.verifyPayment);
 
 // Payment status
-router.get('/payment-status/:orderId',PaymentController.paymentStatus)
+router.get("/payment-status/:orderId", PaymentController.paymentStatus);
 
 // refund
 // router.post('/refund', PaymentController.refundPayment);
