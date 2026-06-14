e8791df7b744973da719ff980c69b57028efa032
policyplanner1 <it@policyplanner.com>
2026-06-15 00:55:24 +0530

    reward phase-5

--- server/app/ecommerce/v1/controllers/productController.js
+++ server/app/ecommerce/v1/controllers/productController.js
@@ -6,27 +6,6 @@ const path = require("path");
 const CDN_BASE_URL = "https://cdn.rewardplanners.com";
 
 // Helper function
-// function calculateReward(price, product) {
-//   if (!product.can_earn_reward) return 0;
-
-//   if (!product.reward_type) return 0;
-
-//   let reward = 0;
-
-//   if (product.reward_type === "percentage") {
-//     reward = (price * product.reward_value) / 100;
-
-//     if (product.max_reward) {
-//       reward = Math.min(reward, product.max_reward);
-//     }
-//   }
-
-//   if (product.reward_type === "fixed") {
-//     reward = product.reward_value;
-//   }
-
-//   return Math.floor(reward);
-// }
 function calculateReward(orderAmount, rules) {
   if (!rules || !rules.length) return 0;
 
