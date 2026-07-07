436af7ba0909f1eca31d27170e615cc237e21021
sakshi1034 <chavansakshi685@gmail.com>
2026-07-07 15:20:28 +0530

    ticketdatafeching code

--- server/app/common/routes/supportRoute.js
+++ server/app/common/routes/supportRoute.js
@@ -9,4 +9,7 @@ router.get("/categories", SupportController.getCategories);
 // create a new support ticket
 router.post("/create-ticket", auth, SupportController.createTicket);
 
+// get all tickets for logged in user
+router.get("/my-tickets", auth, SupportController.getMyTickets);
+
 module.exports = router;
